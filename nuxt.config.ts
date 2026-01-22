bbcec600758d0b2f28acb7b840350b6aa951570e
Kamsou <camille.coutens@gmail.com>
2026-01-22 09:25:08 +0100

    feat: add Vue.js official logo as favicon
    - Use SVG favicon for better quality and scalability
    - Remove old favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+    },
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
