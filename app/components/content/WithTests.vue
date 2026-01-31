667b91879e0a47df1aabfbc96f075dbce1e7d61a
Nicolas Issalene <nicolas.issalene@zettascale.tech>
2026-01-31 22:53:36 +0100

    lint

--- app/components/content/WithTests.vue
+++ app/components/content/WithTests.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 const items = [
   {
-    label: "How to use",
-    icon: "i-lucide-pencil",
-    slot: "use",
+    label: 'How to use',
+    icon: 'i-lucide-pencil',
+    slot: 'use',
   },
   {
-    label: "How to test",
-    icon: "i-lucide-test-tube-diagonal",
-    slot: "test",
+    label: 'How to test',
+    icon: 'i-lucide-test-tube-diagonal',
+    slot: 'test',
   },
-];
+]
 </script>
+
 <template>
   <UTabs :items="items">
     <template #use>
--- tests/button.spec.ts
+++ tests/button.spec.ts
@@ -1,28 +1,28 @@
-import { describe, test, expect } from "vitest";
-import { mount } from "@vue/test-utils";
-import { defineComponent, ref } from "vue";
+import { describe, test, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { defineComponent, ref } from 'vue'
 
-describe("Button test", () => {
+describe('Button test', () => {
   const SystemUnderTest = defineComponent({
     template: '<button @click="count++">Count: {{ count }}</button>',
     setup: () => {
-      const count = ref(1);
+      const count = ref(1)
 
       return {
         count,
-      };
+      }
     },
-  });
+  })
 
-  test("Should have initial value to 0", async () => {
-    const component = mount(SystemUnderTest);
-    await expect(component.find("button").text()).toEqual("Count: 1");
-  });
-  test("Should increment counter on click", async () => {
-    const component = mount(SystemUnderTest);
+  test('Should have initial value to 0', async () => {
+    const component = mount(SystemUnderTest)
+    await expect(component.find('button').text()).toEqual('Count: 1')
+  })
+  test('Should increment counter on click', async () => {
+    const component = mount(SystemUnderTest)
 
-    await component.find("button").trigger("click");
+    await component.find('button').trigger('click')
 
-    await expect(component.find("button").text()).toEqual("Count: 2");
-  });
-});
+    await expect(component.find('button').text()).toEqual('Count: 2')
+  })
+})
