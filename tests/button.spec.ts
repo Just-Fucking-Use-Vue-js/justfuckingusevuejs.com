852140cfbc3a7be426873c72811981d2fffcc97b
Nicolas Issalene <nicolas.issalene@zettascale.tech>
2026-01-31 22:49:39 +0100

    feat(test-button): Add custom component to provide test aside examples

---
+++ tests/button.spec.ts
@@ -0,0 +1,28 @@
+import { describe, test, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import { defineComponent, ref } from "vue";
+
+describe("Button test", () => {
+  const SystemUnderTest = defineComponent({
+    template: '<button @click="count++">Count: {{ count }}</button>',
+    setup: () => {
+      const count = ref(1);
+
+      return {
+        count,
+      };
+    },
+  });
+
+  test("Should have initial value to 0", async () => {
+    const component = mount(SystemUnderTest);
+    await expect(component.find("button").text()).toEqual("Count: 1");
+  });
+  test("Should increment counter on click", async () => {
+    const component = mount(SystemUnderTest);
+
+    await component.find("button").trigger("click");
+
+    await expect(component.find("button").text()).toEqual("Count: 2");
+  });
+});
